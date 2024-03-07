323ed148945f7a1f76d97aec9eeb1463a8c197c1
NgocTuan <doanngoctuan27@gmail.com>
2024-03-07 12:13:30 +0700

    Code Checkout Screen

---
+++ lib/common/widgets/products/cart/coupon_widget.dart
@@ -0,0 +1,50 @@
+import 'package:TShop/common/widgets/custom_shapes/containers/rounded_container.dart';
+import 'package:TShop/utils/constants/colors.dart';
+import 'package:TShop/utils/constants/size.dart';
+import 'package:TShop/utils/helpers/helper_functions.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class TCouponCode extends StatelessWidget {
+  const TCouponCode({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final dark = THelperFunctions.isDarkMode(context);
+    return TRoundedContainer(
+      showBorder: true,
+      backgroundColor: dark ? TColors.dark: TColors.white,
+      padding: const EdgeInsets.only(top: TSizes.sm, bottom: TSizes.sm, right: TSizes.sm, left: TSizes.md),
+      child: Row(
+        children: [
+          Flexible(
+            child: TextFormField(
+              decoration: const InputDecoration(
+                hintText: 'Have a coupon code? Enter here',
+                focusedBorder: InputBorder.none,
+                enabledBorder: InputBorder.none,
+                errorBorder: InputBorder.none,
+                disabledBorder: InputBorder.none,
+              ),
+            ),
+          ),
+    
+          // Button
+          SizedBox(
+            width: 80,
+            child: ElevatedButton(
+              onPressed: (){}, 
+              style: ElevatedButton.styleFrom(
+                foregroundColor: dark ? TColors.white.withOpacity(0.5) : TColors.dark.withOpacity(0.5),
+                backgroundColor: Colors.grey.withOpacity(0.2),
+                side: BorderSide(color: Colors.grey.withOpacity(0.1)),
+              ),
+              child: const Text('Apply'))
+          ),
+        ],
+      )
+    );
+  }
+}
--- lib/features/shop/screens/cart/cart.dart
+++ lib/features/shop/screens/cart/cart.dart
@@ -1,19 +1,11 @@
 
 import 'package:TShop/common/widgets/appbar/appbar.dart';
-import 'package:TShop/common/widgets/icons/t_circular_icon.dart';
-import 'package:TShop/common/widgets/images/t_rounded_image.dart';
-import 'package:TShop/common/widgets/products/cart/add_remove_button.dart';
-import 'package:TShop/common/widgets/products/cart/cart_item.dart';
-import 'package:TShop/common/widgets/texts/product_price_text.dart';
-import 'package:TShop/common/widgets/texts/product_title_text.dart';
-import 'package:TShop/utils/constants/colors.dart';
-import 'package:TShop/utils/constants/image_string.dart';
+import 'package:TShop/features/shop/screens/cart/widgets/cart_items.dart';
+import 'package:TShop/features/shop/screens/checkout/checkout.dart';
 import 'package:TShop/utils/constants/size.dart';
-import 'package:TShop/utils/helpers/helper_functions.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-import 'package:iconsax/iconsax.dart';
+import 'package:get/get.dart';
+
 
 class CartScreen extends StatelessWidget {
   const CartScreen({Key? key}) : super(key: key);
@@ -22,40 +14,19 @@ class CartScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
        appBar: TAppBar(showBackArrow: true, title: Text('Cart', style: Theme.of(context).textTheme.headlineSmall)),
-       body: Padding(
-         padding: const EdgeInsets.all(TSizes.defaultSpace),
-         child: ListView.separated(
-           shrinkWrap: true,
-           itemCount: 10,
-           separatorBuilder: (_, __) => const SizedBox(height: TSizes.spaceBtwSections),
-           itemBuilder: (_, index) => const Column(
-             children: [
-               TCartItem(),
-               SizedBox(height: TSizes.spaceBtwItems),
-       
-               Row(
-                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                 children: [
-                   Row(
-                     children: [
-                       SizedBox(width: 70),
-                       TProductQuantityWidthAddRemoveButton(),
-                     ],
-                   ),
-                   //Add Remove Button
-                   TProductPriceText(price: '590'),
-                 ],
-               )
-             ],
-           ),
-         ),
+       body: const Padding(
+         padding: EdgeInsets.all(TSizes.defaultSpace),
+
+         // Items in Cart
+         child: TCartItems(),
        ),
+
+       //Checkout Button
        bottomNavigationBar: Padding(
          padding: const EdgeInsets.all(TSizes.defaultSpace),
-         child: ElevatedButton(onPressed: (){}, child: const Text('Checkout \$590')),
+         child: ElevatedButton(onPressed: () => Get.to(() => const CheckoutScreen()), child: const Text('Checkout \$590')),
        ),
       );
   }
 }
-
 

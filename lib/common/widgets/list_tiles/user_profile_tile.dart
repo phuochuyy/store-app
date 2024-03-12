79128c31a2870bc72380e998dc6e15690cc8cf10
nph17103 <nph17103@gmail.com>
2024-03-12 14:42:53 +0700

    Code profile screen UI

--- lib/common/widgets/list_tiles/user_profile_tile.dart
+++ lib/common/widgets/list_tiles/user_profile_tile.dart
@@ -27,7 +27,7 @@ class TUserProfileTile extends StatelessWidget {
             Theme.of(context).textTheme.bodyMedium!.apply(color: TColors.white),
       ),
       trailing: IconButton(
-          onPressed: () {},
+          onPressed: onPressed,
           icon: const Icon(
             Iconsax.edit,
             color: TColors.white,

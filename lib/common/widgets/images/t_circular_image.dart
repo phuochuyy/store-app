d92a2fd5c25c3271c221bcce54050f3d4a2c8d81
huy <nph17103@gmail.com>
2024-05-07 05:23:49 +0700

    Upload Images To Firebase Storage

--- lib/common/widgets/images/t_circular_image.dart
+++ lib/common/widgets/images/t_circular_image.dart
@@ -1,6 +1,8 @@
 import 'package:TShop/utils/constants/colors.dart';
 import 'package:TShop/utils/constants/size.dart';
+import 'package:TShop/utils/custom_shimmer/TShimmerEffect.dart';
 import 'package:TShop/utils/helpers/helper_functions.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 class TCircularImage extends StatelessWidget {
@@ -35,13 +37,23 @@ class TCircularImage extends StatelessWidget {
                 : TColors.white),
         borderRadius: BorderRadius.circular(100),
       ),
-      child: Center(
-        child: Image(
-          fit: fit,
-          image: isNetworkImage
-              ? NetworkImage(image)
-              : AssetImage(image) as ImageProvider,
-          color: overlayColor,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(100),
+        child: Center(
+          child: isNetworkImage
+              ? CachedNetworkImage(
+                  imageUrl: image,
+                  fit: fit,
+                  color: overlayColor,
+                  progressIndicatorBuilder: (context, url, downloadProgress) =>
+                      const TShimmerEffect(width: 55, height: 55, radius: 55),
+                  errorWidget: (context, url, error) => const Icon(Icons.error),
+                )
+              : Image(
+                  fit: fit,
+                  image: AssetImage(image),
+                  color: overlayColor,
+                ),
         ),
       ),
     );

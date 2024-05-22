e8ba72a0594ff779c7e0f3599ce585273aff32ba
HungTran24 <tranquocphong123452401@gmail.com>
2024-05-22 16:38:16 +0700

    create category_repository

--- lib/features/shop/models/category_model.dart
+++ lib/features/shop/models/category_model.dart
@@ -18,8 +18,29 @@ class CategoryModel {
   static CategoryModel empty() => CategoryModel(id: '', image: '', name: '', isFeatured: false);
 
 /// Convert model to Json structure so that you can store data in Firebase
- toJson(){
-
+Map<String,dynamic>toJson(){
+  return{
+    'Name': name,
+    'Image': image,
+    'ParentId': parentId,
+    'IsFeatured': isFeatured,
+  };
 }
 /// Map Json oriented document snapshot from Firebase to UserModel
+factory CategoryModel.fromSnapshot (DocumentSnapshot<Map<String, dynamic>> document) {
+    if (document.data() != null) {
+      final data = document.data() !;
+// Map JSON Record to the Model
+      return CategoryModel(
+          id: document.id,
+          name: data['Name'] ?? '',
+          image: data['Image'] ?? '',
+          parentId: data['ParentId'] ?? '',
+          isFeatured: data['IsFeatured'] ?? false,
+      );
+    } else {
+          return CategoryModel.empty();
+    }
+
+ }
 }

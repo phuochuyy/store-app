89eb36373041454ef428dcee117ad4361620d798
NgocTuan <doanngoctuan27@gmail.com>
2024-06-21 15:43:56 +0700

    create AddressModel, CRUD Address

---
+++ lib/data/repositories/address/address_repository.dart
@@ -0,0 +1,44 @@
+
+import 'package:TShop/data/repositories/authentication/authentication_repository.dart';
+import 'package:TShop/features/personalization/models/address_model.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:get/get.dart';
+
+class AddressRepository extends GetxController{
+  static AddressRepository get instance => Get.find();
+
+  final _db = FirebaseFirestore.instance;
+
+  Future<List<AddressModel>> fetchUserAddresses() async {
+    try{
+      final userId = AuthenticationRepository.instance.authUser!.uid;
+      if (userId.isEmpty) throw 'Không tìm thấy người dùng!';
+
+      final result = await _db.collection('Users').doc(userId).collection('Addresses').get();
+      return result.docs.map((documentSnapshot) => AddressModel.fromDocumentSnapshot(documentSnapshot)).toList();
+    }catch(e) {
+      throw 'Có lỗi xảy ra trong quá trình Fetch Address Data.!';
+    }  
+  }
+
+  // Clear the 'selected' field for all addresses
+  Future<void> updateSelectedField(String adddressId, bool selected) async{
+    try{
+      final userId = AuthenticationRepository.instance.authUser!.uid;
+      await _db.collection('Users').doc(userId).collection('Addresses').doc(adddressId).update({'SelectedAddress': selected});
+    } catch(e){
+      throw 'Lỗi khi cập nhật địa chỉ.!';
+    } 
+  }
+
+  // Store new user order
+  Future<String> addAddress(AddressModel address) async{
+    try{
+      final userId = AuthenticationRepository.instance.authUser!.uid;
+      final currentAddress = await _db.collection('Users').doc(userId).collection('Addresses').add(address.toJson());
+      return currentAddress.id;
+    }catch(e){
+      throw 'Xảy ra lưu khi lưu địa chỉ. Vui lòng thử lại!';
+    }
+  }
+}
--- lib/features/personalization/screens/address/widgets/single_address.dart
+++ lib/features/personalization/screens/address/widgets/single_address.dart
@@ -1,72 +1,72 @@
 import 'package:TShop/common/widgets/custom_shapes/containers/rounded_container.dart';
+import 'package:TShop/features/personalization/controllers/address_controller.dart';
+import 'package:TShop/features/personalization/models/address_model.dart';
 import 'package:TShop/utils/constants/colors.dart';
 import 'package:TShop/utils/constants/size.dart';
 import 'package:TShop/utils/helpers/helper_functions.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
 import 'package:iconsax/iconsax.dart';
 
 class TSingleAddress extends StatelessWidget {
-  const TSingleAddress({super.key, required this.selectedAddress});
+  const TSingleAddress({super.key, required this.address, required this.onTap});
 
-  final bool selectedAddress;
-
+  final AddressModel address;
+  final VoidCallback onTap;
   @override
   Widget build(BuildContext context) {
+    final controller = AddressController.instance;
     final dark = THelperFunctions.isDarkMode(context);
-    return TRoundedContainer(
-        width: double.infinity,
-        padding: const EdgeInsets.all(TSizes.md),
-        showBorder: true,
-        backgroundColor: selectedAddress
-            ? TColors.primary.withOpacity(0.5)
-            : Colors.transparent,
-        borderColor: selectedAddress
-            ? Colors.transparent
-            : dark
-                ? TColors.darkGrey
-                : TColors.grey,
-        margin: const EdgeInsets.only(bottom: TSizes.spaceBtwItems),
-        child: Stack(
-          children: [
-            Positioned(
-              right: 5,
-              top: 0,
-              child: Icon(
-                selectedAddress ? Iconsax.tick_circle5 : null,
-                color: selectedAddress
-                    ? dark
-                        ? TColors.light
-                        : TColors.dark
-                    : null,
-              ),
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+    return Obx((){
+      final selectedAddressId = controller.selectedAddress.value.id;
+      final selectedAddress = selectedAddressId == address.id;
+      return InkWell(
+        onTap: onTap,
+        child: TRoundedContainer(
+            width: double.infinity,
+            padding: const EdgeInsets.all(TSizes.md),
+            showBorder: true,
+            backgroundColor: selectedAddress
+                ? TColors.primary.withOpacity(0.5)
+                : Colors.transparent,
+            borderColor: selectedAddress
+                ? Colors.transparent
+                : dark
+                    ? TColors.darkGrey
+                    : TColors.grey,
+            margin: const EdgeInsets.only(bottom: TSizes.spaceBtwItems),
+            child: Stack(
               children: [
-                Text(
-                  'John Doe',
-                  maxLines: 1,
-                  overflow: TextOverflow.ellipsis,
-                  style: Theme.of(context).textTheme.titleLarge,
+                Positioned(
+                  right: 5,
+                  top: 0,
+                  child: Icon(
+                    selectedAddress ? Iconsax.tick_circle5 : null,
+                    color: selectedAddress
+                        ? dark
+                            ? TColors.light
+                            : TColors.dark
+                        : null,
+                  ),
                 ),
-                const SizedBox(
-                  height: TSizes.sm / 2,
-                ),
-                const Text(
-                  '(+123) 456 789',
-                  maxLines: 1,
-                  overflow: TextOverflow.ellipsis,
-                ),
-                const SizedBox(
-                  height: TSizes.sm / 2,
-                ),
-                const Text(
-                  '82356 Timmy Coves, South Liana, Maine, 87665, USA',
-                  softWrap: true,
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      address.name,
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                      style: Theme.of(context).textTheme.titleLarge,
+                    ),
+                    const SizedBox( height: TSizes.sm / 2,),
+                    Text(address.formattedPhoneNo,maxLines: 1,overflow: TextOverflow.ellipsis, ),
+                    const SizedBox( height: TSizes.sm / 2,),
+                    Text(address.toString(), softWrap: true,)
+                  ],
                 )
               ],
-            )
-          ],
-        ));
+            )),
+      );}
+    );
   }
 }

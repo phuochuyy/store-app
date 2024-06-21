cb2597fa03df3e7b23eac43f044db07a30490bb3
Hung <21522127@gm.uit.edu.vn>
2024-06-21 12:52:06 +0700

    Fix bug brand_controller.dart

---
+++ lib/common/shimmer/brand_shimmer.dart
@@ -0,0 +1,14 @@
+import 'package:TShop/common/shimmer/shimmer.dart';
+import 'package:TShop/common/widgets/layouts/grid_layout.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class TBrandsShimmer extends StatelessWidget {
+  const TBrandsShimmer({super.key,  this.itemCount=4});
+  final int itemCount;
+
+  @override
+  Widget build(BuildContext context) {
+    return TGridLayout(itemCount: itemCount, itemBuilder: (_, __) => const TShimmerEffect(width: 300, height: 80), mainAxisExtent: 80,);
+  }
+}

6f2a2f98802ec3d2919c1da1d7169692547b42db
DonKongChys <doancongtrics@gmail.com>
2024-06-27 22:35:57 +0700

    fix conflict hung voi main

--- lib/features/shop/controllers/product/checkout_controller.dart
+++ lib/features/shop/controllers/product/checkout_controller.dart
@@ -11,38 +11,49 @@ import '../../models/payment_method_model.dart';
 class CheckoutController extends GetxController {
   static CheckoutController get instance => Get.find();
 
-  final Rx<PaymentMethodModel> selectedPaymentMethod = PaymentMethodModel.empty().obs;
+  final Rx<PaymentMethodModel> selectedPaymentMethod =
+      PaymentMethodModel.empty().obs;
 
   @override
-  void onInit(){
-    selectedPaymentMethod.value = PaymentMethodModel(name: 'Paypal', image: TImages.paypal);
+  void onInit() {
+    selectedPaymentMethod.value =
+        PaymentMethodModel(name: 'Paypal', image: TImages.paypal);
     super.onInit();
   }
-  Future<dynamic> selectPaymentMethod(BuildContext context)  {
-    return  showModalBottomSheet(
+
+  Future<dynamic> selectPaymentMethod(BuildContext context) {
+    return showModalBottomSheet(
       context: context,
-      builder: (_) =>  SingleChildScrollView(
-      child:  Container(
+      builder: (_) => SingleChildScrollView(
+          child: Container(
         padding: const EdgeInsets.all(TSizes.lg),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            TSectionHeading(title: 'Chọn phương thức thanh toán', showActionButton: false,),
+            TSectionHeading(
+              title: 'Chọn phương thức thanh toán',
+              showActionButton: false,
+            ),
             SizedBox(height: TSizes.spaceBtwSections),
-            TPaymentTile(paymentMethod: PaymentMethodModel(name: 'Paypal', image: TImages.paypal)),
-            const SizedBox(height: TSizes.spaceBtwItems/2),
-            TPaymentTile(paymentMethod: PaymentMethodModel(name: 'Google Pay', image: TImages.google)),
-            const SizedBox(height: TSizes.spaceBtwItems/2),
-            TPaymentTile(paymentMethod: PaymentMethodModel(name: 'Mono', image: TImages.momo)),
-            const SizedBox(height: TSizes.spaceBtwItems/2),
-            TPaymentTile(paymentMethod: PaymentMethodModel(name: 'Visa', image: TImages.momo)),
-            const SizedBox(height: TSizes.spaceBtwItems/2),
+            TPaymentTile(
+                paymentMethod:
+                    PaymentMethodModel(name: 'Paypal', image: TImages.paypal)),
+            const SizedBox(height: TSizes.spaceBtwItems / 2),
+            TPaymentTile(
+                paymentMethod: PaymentMethodModel(
+                    name: 'Google Pay', image: TImages.google)),
+            const SizedBox(height: TSizes.spaceBtwItems / 2),
+            TPaymentTile(
+                paymentMethod:
+                    PaymentMethodModel(name: 'Mono', image: TImages.momo)),
+            const SizedBox(height: TSizes.spaceBtwItems / 2),
+            TPaymentTile(
+                paymentMethod:
+                    PaymentMethodModel(name: 'Visa', image: TImages.momo)),
+            const SizedBox(height: TSizes.spaceBtwItems / 2),
           ],
         ),
-      )
-      ),
+      )),
     );
-
+  }
 }
-
-
